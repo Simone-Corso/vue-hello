f19c85a33d94418fe54f486a7ed56868ab01d205
Simone-Corso <scorso26@gmail.com>
2023-11-30 16:04:19 +0100

    ho inserito anche title sotto immagine

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const {createApp} = Vue
             data(){
                 return {
                     message: 'Hello Vue',
-                    image: 'https://people.com/thmb/njUuAHakxw0tE6HOFFbW0xMagJ8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(739x81:741x83)/kobe-bryant-cover-nba-2k-070523-tout-24f48f66b19f4bc9ab9427889c6425e0.jpg'
+                    image: 'https://people.com/thmb/njUuAHakxw0tE6HOFFbW0xMagJ8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(739x81:741x83)/kobe-bryant-cover-nba-2k-070523-tout-24f48f66b19f4bc9ab9427889c6425e0.jpg',
+                    title: 'my inspiration'
                 }
             }
         }).mount('#app')
